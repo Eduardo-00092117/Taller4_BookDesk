13b1d702bf24c8a41714c9c6d8e070a66d7f5f32
calderonra <00004017@uca.edu.sv>
2019-05-25 15:35:56 -0600

    Mejoras en ui y agrega la bookViewer

--- app/src/main/java/com/example/taller4_bookdex/Adapter/BookAdapter.kt
+++ app/src/main/java/com/example/taller4_bookdex/Adapter/BookAdapter.kt
@@ -6,39 +6,43 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
 import com.example.taller4_bookdesk.Entities.Book
-import com.example.taller4_bookdesk.R
+import com.example.taller4_bookdex.R
+import kotlinx.android.synthetic.main.cardview_book.view.*
 
-/*
-class BookAdapter internal constructor(
-    context: Context
-) : RecyclerView.Adapter<BookAdapter.Bookviewholder>(){
+class BooksAdapter internal  constructor(context: Context, val clickListenerBoton: (Book) -> Unit, val clickListenerViewHolder: (Book)->Unit ) : RecyclerView.Adapter<BooksAdapter.ViewHolder>() {
+    private var books: List<Book> = emptyList()
+    public var switch = true
 
-    private val inflater: LayoutInflater = LayoutInflater.from(context)
-    private var books = emptyList<Book>()
-
-    inner class BookViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val bookItemView: TextView = itemView.findViewById(R.id.textView)
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.cardview_book, parent, false)
+        return ViewHolder(view)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
-        val itemView = inflater.inflate(R.layout.cardview_book, parent, false)
-        return BookViewHolder(itemView)
-    }
+    override fun getItemCount() = books.size
 
-     override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(books[position], clickListener)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(books[position], clickListenerBoton,clickListenerViewHolder )
 
-
-     internal fun setBooks(books: List<Book>) {
+    internal fun setBooks(books: List<Book>) {
         this.books = books
         notifyDataSetChanged()
     }
 
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    override fun getItemCount() = books.size
+        fun bind(book: Book, clickListenerBoton: (Book) -> Unit, clickListenerViewHolder:(Book) -> Unit) = with(itemView) {
+            Glide.with(itemView.context)
+                .load(book.titulo)
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(Iv_book_portada)
+            Tv_book_name.text = book.titulo
 
+            this.setOnClickListener(){
+                clickListenerViewHolder(book)
+            }
 
+        }
+    }
 }
-
-*/
--- app/src/main/java/com/example/taller4_bookdex/MainActivity.kt
+++ app/src/main/java/com/example/taller4_bookdex/MainActivity.kt
@@ -1,40 +1,69 @@
 package com.example.taller4_bookdex
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.taller4_bookdesk.ViewModel.BookRepoViewModel
+import com.example.taller4_bookdesk.Adapter.BooksAdapter
+import com.example.taller4_bookdesk.Entities.Book
+import com.example.taller4_bookdex.ViewModel.BookRepoViewModel
 import java.util.Observer
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var bookRepoViewModel:BookRepoViewModel
+    private lateinit var bookRepoViewModel: BookRepoViewModel
+    private lateinit var bookAdapter: BooksAdapter
+
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bookAdapter = BooksAdapter(this, { book: Book -> bookFavorito(book) }, { book: Book -> triggerActivity(book) })
 
 
 
-        /*
-        bookRepoViewModel=BookRepoViewModelProviders.of(this).get(BookRepoViewModel::class.java)
 
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
-        val adapter = BookAdapter
-        recyclerView.adapter = adapter
+
+
+        val recyclerView = findViewById<RecyclerView>(R.id.list_books)
+        recyclerView.adapter = bookAdapter
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        BookRepoViewModel.allBooks.observe(this, Observer { books ->
-            // Update the cached copy of the words in the adapter.
-            books?.let { adapter.setBooks(it) }
-            })
+        bookRepoViewModel = ViewModelProviders.of(this).get(BookRepoViewModel::class.java)
 
+        bookRepoViewModel.allBooks.observe(this,androidx.lifecycle.Observer { book->{
+            bookAdapter.setBooks(book)
+        } })
+
+
+
+
+
+
+
+    }
+
+    private fun triggerActivity(book: Book) {
+/*
+        val libroBundle = Bundle()
+        libroBundle.putParcelable("LIBRO", book)
+        startActivity(Intent(this, BookViewerActivity::class.java).putExtras(libroBundle))
+*/
+    }
+
+    private fun bookFavorito(book: Book) {
+        /*
+        bookViewModel.favBooks.observe(this, Observer { favBooks ->
+            bookAdapter.setBooks(favBooks)
+
+        })
+        bookViewModel.marcarODesmarcarFav(book)
         */
-
     }
 
 
